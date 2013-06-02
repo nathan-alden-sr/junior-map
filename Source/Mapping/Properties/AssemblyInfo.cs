bf7aa04537f33af7b9c081983d0fd70ab200b74e
Nathan Alden, Sr <me@nathanalden.com>
2013-06-01 20:51:17 -0500

    Updated version number

--- Source/Mapping/Properties/AssemblyInfo.cs
+++ Source/Mapping/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Map")]
-[assembly:AssemblyVersion("2.0.0.0")]
-[assembly:AssemblyFileVersion("2.0.1.0")]
+[assembly:AssemblyVersion("2.1.0.0")]
+[assembly:AssemblyFileVersion("2.1.0.0")]
