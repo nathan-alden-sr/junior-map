dae2aa0fb183a798da166d48ce3c95ba8d29c7b8
Nathan Alden, Sr <me@nathanalden.com>
2014-09-16 14:20:46 -0400

    Updated Junior.Common

--- Source/Mapping/Properties/AssemblyInfo.cs
+++ Source/Mapping/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Map")]
-[assembly:AssemblyVersion("6.0.0.0")]
-[assembly:AssemblyFileVersion("6.0.1.0")]
+[assembly:AssemblyVersion("7.0.0.0")]
+[assembly:AssemblyFileVersion("7.0.0.0")]
